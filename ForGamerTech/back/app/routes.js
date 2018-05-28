833435ea980090546831678ffd7af9dd4bfa33bc
Palito <35345583+Palito9007@users.noreply.github.com>
2018-05-28 18:02:28 +0100

    v.0.1 Login

--- ForGamerTech/back/app/routes.js
+++ ForGamerTech/back/app/routes.js
@@ -1,3 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    }
+  ]
+})
+
+
 module.exports = function(app, passport) {
 
     // =====================================
--- ForGamerTech/front/pages/login.vue
+++ ForGamerTech/front/pages/login.vue
@@ -5,10 +5,10 @@
 <div class="container login-wrapper border border-light">
     <form class="form-signin" @submit.prevent="login">
       <h2 class="form-signin-heading">Formulário de Login</h2>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <label for="inputEmail" class="sr-only">Insira o seu E-mail</label>
+      <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Insira o seu E-mail" required autofocus>
+      <label for="inputPassword" class="sr-only">Insira a respectiva password</label>
+      <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Insira a respectiva password" required>
       <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
     </form>
   </div>
@@ -25,16 +25,64 @@
 <script>
 export default {
   layout: 'sidenav',
-  methods:{
+  name: 'Login',
+  data () {
+    return {
+      email: '',
+      password: ''
     }
-
-
-
-    
+  },
+  methods: {
+    login () {
+      console.log(this.email)
+      console.log(this.password)
+    }
   }
+}
 </script>
 
+<style lang="css">
+body {
+  background: #605B56;
+}
 
-<style>
+.login-wrapper {
+  background: #fff;
+  width: 70%;
+  margin: 12% auto;
+}
 
+.form-signin {
+  max-width: 330px;
+  padding: 10% 15px;
+  margin: 0 auto;
+}
+.form-signin .form-signin-heading,
+.form-signin .checkbox {
+  margin-bottom: 10px;
+}
+.form-signin .checkbox {
+  font-weight: normal;
+}
+.form-signin .form-control {
+  position: relative;
+  height: auto;
+  -webkit-box-sizing: border-box;
+          box-sizing: border-box;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
 </style>
